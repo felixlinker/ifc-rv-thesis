743447ae78f0f002d24620c767bb65d806e79b06
felixlinker <linkerfelix@gmail.com>
2019-06-21 13:08:46 +0200

    Add script to pretty-print traces to html

---
+++ trace.ps1
@@ -0,0 +1,28 @@
+param(
+    [string]$in = ".\model\min-rv.smv",
+    [string]$out = ".\dist\trace.html",
+    [string]$cmd = ".\trace-bmc.template",
+    [string]$prop
+)
+
+# Create temporary files needed in this script
+$preprocessed = New-TemporaryFile
+$trace = New-TemporaryFile
+$replaced_cmd = New-TemporaryFile
+
+# Preprocess the model with the WSL c-preprocessor
+bash -c "cpp $in".replace("\", "/") `
+    | Out-File -Encoding ascii $preprocessed.FullName
+
+# Replace template-like variables in the command file
+$cmds = get-content $cmd
+$cmds = $cmds.Replace("[INPUT]", $preprocessed.FullName)
+$cmds = $cmds.Replace("[PROPNAME]", $prop)
+$cmds = $cmds.Replace("[OUTPUT]", $trace.FullName)
+$cmds | Out-File -encoding ascii $replaced_cmd.FullName
+
+# Run nuXmv
+nuXmv.exe -source $replaced_cmd.FullName
+
+# Map the trace to an html table
+smvtrcviz.bat $trace.FullName $out
