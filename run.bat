49ddd531b9f8aa52d4a441a603496474c2f01f34
felixlinker <linkerfelix@gmail.com>
2019-06-25 19:07:42 +0200

    model: Unify scripts

--- run.bat
+++
@@ -1,3 +0,0 @@
-set input=model/min-rv
-bash -c "cpp %input%.smv > %input%.o.smv"
-nuXmv.exe %* %input%.o.smv
--- trace.ps1
+++ trace.ps1
@@ -1,10 +1,17 @@
 param(
     [string]$in = ".\model\min-rv.smv",
-    [string]$out = ".\dist\trace.html",
+    [string]$outDir = ".\dist",
     [string]$cmd = ".\trace-bmc.template",
     [int]$tracelen = 10,
-    [Parameter(Mandatory=$True)]
-    [string]$prop
+    [string[]]$props = @(
+        "NO_LEAK",
+        "NO_LEAK_SANITIZED",
+        "NO_LEAK_SANITIZED_NO_MEM_DECLASSIFICATION",
+        "NO_LEAK_SANITIZED_NO_USE_DECLASSIFIED_MEM",
+        "NO_INFLUENCE",
+        "NO_INFLUENCE_NO_PUBLIC_READS",
+        "NO_INFLUENCE_NO_PUBLIC_READS_SANITIZED"
+    )
 )
 
 # Create temporary files needed in this script
@@ -16,19 +23,22 @@ $replaced_cmd = New-TemporaryFile
 bash -c "cpp $in".replace("\", "/") `
     | Out-File -Encoding ascii $preprocessed.FullName
 
-# Replace template-like variables in the command file
-$cmds = get-content $cmd
-$cmds = $cmds.Replace("[INPUT]", $preprocessed.FullName)
-$cmds = $cmds.Replace("[PROPNAME]", $prop)
-$cmds = $cmds.Replace("[OUTPUT]", $trace.FullName)
-$cmds = $cmds.Replace("[MAXLEN]", $tracelen)
-$cmds | Out-File -encoding ascii $replaced_cmd.FullName
+foreach ($prop in $props) {
+    # Replace template-like variables in the command file
+    $cmds = get-content $cmd
+    $cmds = $cmds.Replace("[INPUT]", $preprocessed.FullName)
+    $cmds = $cmds.Replace("[PROPNAME]", $prop)
+    $cmds = $cmds.Replace("[OUTPUT]", $trace.FullName)
+    $cmds = $cmds.Replace("[MAXLEN]", $tracelen)
+    $cmds | Out-File -encoding ascii $replaced_cmd.FullName
 
-# Run nuXmv
-nuXmv.exe -source $replaced_cmd.FullName
+    # Run nuXmv
+    nuXmv.exe -source $replaced_cmd.FullName
 
-# Map the trace to an html table
-smvtrcviz.bat $trace.FullName $out
+    # Map the trace to an html table
+    $out = Join-Path $outDir ($prop + ".html")
+    smvtrcviz.bat $trace.FullName $out
+}
 
 # Clean up temporary files
 Remove-Item -Path $preprocessed
